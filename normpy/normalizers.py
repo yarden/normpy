3578e02ddbb2087fcd4d5d72e2095db8c492db89
Yarden Katz <yarden.katz@gmail.com>
2013-07-02 20:33:32 -0400

    Sketches

--- normpy/normalizers.py
+++ normpy/normalizers.py
@@ -1,11 +1,33 @@
 ##
 ## Normalizers
 ##
+import numpy as np
+import scipy
 
+# Wrapper functions for normalizers
+def norm_qc():
+    pass
+
+
+def norm_tmm():
+    pass
+
+
+def norm_tc():
+    pass
+
+
+def norm_lowess():
+    pass
+
+
+def norm_rpkm():
+    pass
 
 # Methods:
 #  UQ (Upper Quartile)
 #  TMM (Trimmed Mean of M values)
 #  TC (Total counts)
+#  LOWESS
 #  [optional] RPKM (RPKM)
 
