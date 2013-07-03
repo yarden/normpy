156bd6bc5e5f918c349f532dcc7c2adb5c6f0eb6
Yarden Katz <yarden.katz@gmail.com>
2013-07-03 07:53:50 -0400

    Pasilla test data set

--- normpy/experiment.py
+++ normpy/experiment.py
@@ -1,10 +1,28 @@
 ##
 ## Representation of an Experiment and a set of
-## samples.  Keep it simple
+## samples.  Keep it simple.
 ##
+from collections import OrderedDict
 
 class Experiment:
-    def __init__(self):
-        pass
+    """
+    Representation of an Experiment, which involves a set
+    of Samples and technical parameters of the experiment
+    (experimental design, the type of sequencing reads, etc.)
+    """
+    def __init__(self, samples,
+                 reads_type="single",
+                 exp_type=None):
+        """
+        Initialize experiments.
+
+        - samples: samples in the experiment
+        - reads_type: single-end or paired-end
+        - exp_type: The experimental design type. Not used
+        for now.
+        """
+        self.samples = samples
+        self.reads_type = reads_type
+        self.exp_type = exp_type
 
 
--- normpy/sample.py
+++ normpy/sample.py
@@ -1,8 +1,12 @@
 ##
 ## Representation of an RNA-Seq sample and its associated counts files
 ##
+from collections import defaultdict
 
 class Sample:
+    """
+    An RNA-Seq sample.
+    """
     def __init__(self, label, counts_fname):
         self.label = label
         self.counts_fname = counts_fname
